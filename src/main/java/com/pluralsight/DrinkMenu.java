f2308ef2e1948281d8a6327e1ea2abfea99a2f9f
silv3r <cyb3rsilv3r@gmail.com>
2026-05-24 08:26:35 -0400

    added DrinkMenu

---
+++ src/main/java/com/pluralsight/DrinkMenu.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public enum DrinkMenu {
+    COKE(2.00, 2.50, 3.00),
+    SPRITE(2.00, 2.50, 3.00),
+    FANTA(2.00, 2.50, 3.00),
+    DR_PEPPER(2.00, 2.50, 3.00),
+    LEMONADE(2.00, 2.50, 3.00);
+
+    private final double smallPrice;
+    private final double mediumPrice;
+    private final double largePrice;
+
+    DrinkMenu(double smallPrice, double mediumPrice, double largePrice) {
+
+        this.smallPrice = smallPrice;
+        this.mediumPrice = mediumPrice;
+        this.largePrice = largePrice;
+    }
+}
--- src/main/java/com/pluralsight/DrinkSize.java
+++
@@ -1,8 +0,0 @@
-package com.pluralsight;
-
-public enum DrinkSize {
-    SMALL(2.00),
-    MEDIUM(2.50),
-    LARGE(3.00)
-
-}
