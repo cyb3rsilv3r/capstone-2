e56d3a3ce5ee21539b35433edac8df4c01d38fd6
silv3r <cyb3rsilv3r@gmail.com>
2026-05-26 09:29:27 -0400

    1 SignaturePizza added

--- src/main/java/com/pluralsight/SignaturePizza.java
+++ src/main/java/com/pluralsight/SignaturePizza.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
-public class SignaturePizza {
+public class SignaturePizza extends Pizza {
+
+    private String signatureName;
+
+    public SignaturePizza(String signatureName) {
+
+        super(PizzaSize.LARGE, CrustType.REGULAR, false, false);
+
+        this.signatureName = signatureName;
+
+        if (signatureName.equalsIgnoreCase("Margherita")) {
+
+            addSauce(Sauces.MARINARA);
+            addSauce(Sauces.OLIVE_OIL);
+
+            addPremiumTopping(PremiumToppings.MOZZARELLA);
+
+            addRegularTopping(RegularToppings.TOMATOES);
+            addRegularTopping(RegularToppings.BASIL);
+        }
+
+    }
 }
